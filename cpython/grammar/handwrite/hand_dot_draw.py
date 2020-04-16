0f46722e05d1ab348a4540e0cfb380cd3d15bb47
yongcai <yongcai@tencent.com>
2020-04-16 13:29:31 +0800

    add support to output to file

--- cpython/grammar/handwrite/hand_dot_draw.py
+++ cpython/grammar/handwrite/hand_dot_draw.py
@@ -1,3 +1,5 @@
+import sys
+
 def gen_dot(rulename, states):
     # rulename: name of rule
     # states: [state0, arc0, state1, arc1,...,staten]
@@ -15,27 +17,40 @@ def gen_dot(rulename, states):
     print("}")
 
 
-def gen_dot_by_arcs(rulename, arcs_type):
+def gen_dot_by_arcs(rulename, rule_text,  arcs_type, file=None):
+    need_close = False
+    if isinstance(file, str):
+        file = open(file, "w")
+        need_close = True
+    if file is None:
+        file = sys.stdout
     # arcs_type  : [([arcs], "type"),...]
-    print("digraph", rulename, "{")
+    print("digraph", rulename, "{", file=file)
+    print(' graph[label="' + rulename + ":" + rule_text + '"]', file=file)
     for arcs, t in arcs_type:
-        gen_dot_by_arcs_type(rulename, arcs, t)
-    print("}")
+        gen_dot_by_arcs_type(rulename, arcs, t, file)
+    print("}", file=file)
+    if need_close:
+        file.close()
 
-def gen_dot_by_arcs_type(rulename, allArcs, t="nfa"):
-    print(" subgraph", rulename+"_"+t, "{")
+def gen_dot_by_arcs_type(rulename, allArcs, t="nfa", file=None):
+    print(" subgraph", rulename+"_"+t, "{", file=file)
     state_id = dict()
+    final_state = set()
     for arc in allArcs:
-        print("  ", end="")
+        print("  ", end="", file=file)
         s, e, a = arc
         sid = state_id.setdefault(s, len(state_id))
         eid = state_id.setdefault(e, len(state_id))
         a = a if a else "epsilon"
         if t == "dfa":
-            print(f'{t}_{sid}->{t}_{eid}[label={a}{",color=red" if e.isfinal else ""}]')
+            print(f'{t}_{sid}->{t}_{eid}[label="{a}"{",color=red" if e.isfinal else ""}]',file=file)
+            if e.isfinal:
+                final_state.add(eid)
         else:
-            print(f'{t}_{sid}->{t}_{eid}[label={a}]')
+            print(f'{t}_{sid}->{t}_{eid}[label="{a}"]', file=file)
 
+    for e in final_state:
+        print(f'  dfa_{e}[color=red]', file=file)
 
-    print("  }")
-
+    print("  }", file=file)
