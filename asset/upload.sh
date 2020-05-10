cd34fe76ee6c75a12768b196d0fb82dcabffbb72
WinChua <winchua@foxmail.com>
2020-05-10 14:31:42 +0800

    up pic

--- asset/upload.sh
+++ asset/upload.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 cur_dir=`dirname  ${0}`
 cd  ${cur_dir}
+last=""
 while [ $# -gt 0 ]
 do
-    cp  $1 .
+    if [ $1 == *Application ]
+    then
+        last=${1}
+        continue
+    fi
+    if [ -n ${last} ]
+    then
+        src="${last} ${1}"
+    else
+        src="${1}"
+    fi
+    cp  "$src" .
     base=`basename $1`
     echo https://github.com/WinChua/blog/blob/master/asset/${base}
     shift
