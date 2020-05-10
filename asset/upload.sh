75ddea3dc5d0d27a4e0fbfb69fc1ffe6cff35d93
WinChua <winchua@foxmail.com>
2020-05-10 14:40:13 +0800

    up pic

--- asset/upload.sh
+++ asset/upload.sh
@@ -3,7 +3,7 @@ cur_dir=`dirname  ${0}`
 cd  ${cur_dir}
 while [ $# -gt 0 ]
 do
-    cp  "$1" .
+    mv "$1" .
     base=`basename "$1"`
     echo https://github.com/WinChua/blog/blob/master/asset/${base}?raw=true
     shift
